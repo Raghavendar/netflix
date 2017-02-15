285cb0fd98e6e1b2ac0fd81e6b2b6af68190b1ea
atiwari54 <atiwari54@csc.com>
2017-02-15 18:19:52 +0530

    code update

--- AT.zuul/com.test.zuul-gateway/src/main/java/com/csc/fs/ngpa/zuul/main/App.java
+++ AT.zuul/com.test.zuul-gateway/src/main/java/com/csc/fs/ngpa/zuul/main/App.java
@@ -1,11 +1,15 @@
 package com.csc.fs.ngpa.zuul.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAutoConfiguration
 public class App 
 {
     public static void main( String[] args )
